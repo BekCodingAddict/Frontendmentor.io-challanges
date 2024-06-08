12decdfa165ba48576ee7829eead669b54e5e117
BekCodingAddict <experte001@gmail.com>
2024-06-09 00:56:31 +0900

    Error handling operation has been done!

--- contact-form/src/ContactForm.jsx
+++ contact-form/src/ContactForm.jsx
@@ -1,21 +1,29 @@
 import { useReducer } from "react";
+import { useForm } from "react-hook-form";
 
 const initialState = {
   generalEnquiry: false,
   supportRequest: false,
   queryTypeError: false,
-  fristNameError: false,
-  lastNameError: false,
-  messageError: false,
 };
 const reducer = (state, action) => {
   switch (action.type) {
     case "generalEnquiry/checked":
-      return { ...state, generalEnquiry: true, supportRequest: false };
+      return {
+        ...state,
+        generalEnquiry: true,
+        supportRequest: false,
+      };
     case "supportRequest/checked":
-      return { ...state, generalEnquiry: false, supportRequest: true };
+      return {
+        ...state,
+        generalEnquiry: false,
+        supportRequest: true,
+      };
     case "queryType/unselected":
       return { ...state, queryTypeError: true };
+    case "queryType/selected":
+      return { ...state, queryTypeError: false };
     default:
       return state;
   }
@@ -25,8 +33,13 @@ function ContactForm() {
   const [{ generalEnquiry, supportRequest, queryTypeError }, dispatch] =
     useReducer(reducer, initialState);
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = () => {
     try {
       const inputData = new FormData(event.target);
       const data = {
@@ -40,7 +53,12 @@ function ContactForm() {
           : null,
         massage: inputData.get("message"),
       };
-      console.log(data);
+
+      data.queryType === null
+        ? dispatch({ type: "queryType/unselected" })
+        : dispatch({ type: "queryType/selected" });
+
+      console.log("data:", data);
       console.log("Data SUbmited!");
     } catch (error) {
       alert(error.message);
@@ -49,31 +67,71 @@ function ContactForm() {
 
   return (
     <div className="container">
-      <form action="" className="form-contaner" onSubmit={handleSubmit}>
+      <form
+        action=""
+        className="form-contaner"
+        onSubmit={handleSubmit(onSubmit)}
+      >
         <h2>Contact Us</h2>
         <div className="row-inputs">
           <div className="column-inputs">
             <label htmlFor="fristname">Frist Name *</label>
-            <input type="text" id="fristname" name="fristName" />
-            <span className="error" id="firstNameError">
-              This field is required
-            </span>
+            <input
+              type="text"
+              id="fristname"
+              name="fristName"
+              {...register("fristName", { required: true, maxLength: 20 })}
+            />
+            {errors?.fristName?.type === "required" && (
+              <span className="error" id="firstNameError">
+                This field is required
+              </span>
+            )}
+
+            {errors?.fristName?.type === "maxLength" && (
+              <span className="error" id="firstNameError">
+                First name cannot exceed 20 characters
+              </span>
+            )}
           </div>
+
           <div className="column-inputs">
             <label htmlFor="lastName">Last Name *</label>
-            <input type="text" id="lastName" name="lastName" />
-            <span className="error" id="lastNameError">
-              This field is required
-            </span>
+            <input
+              type="text"
+              id="lastName"
+              name="lastName"
+              {...register("lastName", { required: true, maxLength: 20 })}
+            />
+
+            {errors?.lastName?.type === "required" && (
+              <span className="error" id="firstNameError">
+                This field is required
+              </span>
+            )}
+
+            {errors?.lastName?.type === "maxLength" && (
+              <span className="error" id="firstNameError">
+                First name cannot exceed 20 characters
+              </span>
+            )}
           </div>
         </div>
 
         <div className="emailInput">
           <label htmlFor="email">Email Address *</label>
-          <input type="email" id="email" name="email" />
-          <span className="error" id="emailError">
-            Please enter a valid email address
-          </span>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            {...register("email", { required: "Email Address is required" })}
+            aria-invalid={errors.mail ? "true" : "false"}
+          />
+          {errors.email && (
+            <span className="error" id="emailError">
+              Please enter a valid email address
+            </span>
+          )}
         </div>
 
         <label htmlFor="generalenquiry">Query Type *</label>
@@ -96,11 +154,12 @@ function ContactForm() {
               id="supportrequest"
               checked={supportRequest}
               onChange={() => dispatch({ type: "supportRequest/checked" })}
+              {...register("queryType", { required: queryTypeError })}
             />
             <label htmlFor="supportrequest">Support Request</label>
           </div>
         </div>
-        {queryTypeError && (
+        {errors?.queryType?.type === "required" && (
           <span className="error" id="queryTypeError">
             Please select a query type
           </span>
@@ -108,10 +167,17 @@ function ContactForm() {
 
         <div className="message">
           <label htmlFor="message">Message *</label>
-          <textarea name="message" id="message" rows="4"></textarea>
-          <span className="error" id="messageError">
-            This field is required
-          </span>
+          <textarea
+            name="message"
+            id="message"
+            rows="4"
+            {...register("message", { required: true })}
+          ></textarea>
+          {errors?.message?.type === "required" && (
+            <span className="error" id="messageError">
+              This field is required
+            </span>
+          )}
         </div>
 
         <div className="checkbox">
